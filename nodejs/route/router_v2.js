f5ab06a05e387e1bcf189477ca98821967202707
7techtf <teinfai.cheng@vigrab.com>
2023-03-21 16:52:07 +0800

    remove unneccsary

--- nodejs/route/router_v2.js
+++ nodejs/route/router_v2.js
@@ -1,8 +1,4 @@
-const example_controller = require("../controller/example_controller")
-const abc = require("../controller/abc")
 const carpark = require("../controller/carpark")
-const mailtrap = require("../controller/mailtrap")
-const credential = require("../controller/credential")
 const express = require('express');
 const router = express.Router();
 
@@ -12,15 +8,6 @@ router.get('/display_medium', carpark.display_medium)
 router.get('/display_large', carpark.display_large)
 router.get('/display_big', carpark.display_big)
 
-router.post('/signup', credential.signup)
-router.post('/create_user', abc.create_user)
-router.post('/mailtrap', mailtrap.mailtrap)
-router.post('/signin', abc.signin)
-router.post('/update', abc.update)
-// router.post('/update', abc.update)
-router.post('/delete', abc.delete)
-router.post('/get_data', abc.get_data)
-router.get('/send', example_controller.sendEmail)
-router.get('/query_example', example_controller.query_example)
+
 
 module.exports = router
--- vuetest/src/router.js
+++ vuetest/src/router.js
@@ -6,17 +6,12 @@ const routes = [
         name: 'home',
         component: () => import('@/components/carpark.vue'),
     },
-    {
-        path: '/signup',
-        name: 'signup',
-        component: () => import('@/components/signup.vue'),
-    }
 ]
 
 const router = createRouter({
-	history: createWebHistory(),
-	routes,
-	linkActiveClass: "active",
+    history: createWebHistory(),
+    routes,
+    linkActiveClass: "active",
 });
 
 export default router
